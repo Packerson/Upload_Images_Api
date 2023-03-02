a349495ba003760a4f60a7cd597c2af94e3d4708
Pawel <pawel.91.kaczmarek@gmail.com>
2023-03-02 10:06:03 +0100

    creat image model

--- Upload_Images/apps/images/admin.py
+++ Upload_Images/apps/images/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Image
 
-# Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'date_creation']
+    list_filter = ['user', 'date_creation', 'title']
+    list_display_links = ['user', 'title']
+
+
+# admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
+
--- Upload_Images/apps/images/models.py
+++ Upload_Images/apps/images/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import validate_image_file_extension
+
 User = get_user_model()
 
 
+def user_directory_path(instance, filename):
+    return 'images/{0}'.format(filename)
+
+
 class Image(models.Model):
-    user = models.OneToOneField(User, related_name="image",
-                                on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="image",
+                             on_delete=models.CASCADE)
     title = models.CharField(max_length=255, unique=True)
-    # image = models.ImageField('Image', validators=[validate_image_file_extension()])
-    image = models.ImageField('Image')
+    alt = models.TextField(null=True)
+    date_creation = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to=user_directory_path)
