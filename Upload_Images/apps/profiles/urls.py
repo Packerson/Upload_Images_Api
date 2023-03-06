16b1ffd02b387219a0c90ad33c4a95752c549984
Pawel <pawel.91.kaczmarek@gmail.com>
2023-03-06 14:39:58 +0100

    solve profile custom_resolution null problem

--- Upload_Images/apps/profiles/urls.py
+++ Upload_Images/apps/profiles/urls.py
@@ -3,17 +3,14 @@ from django.urls import path
 from .views import (BasicPlanListApiView,
                     PremiumPlanListApiView,
                     EnterprisePlanListApiView,
-                    UpdateProfileApiView,
                     GetProfileApiView)
 
 urlpatterns = [
     path("me/", GetProfileApiView.as_view(), name="get_profile"),
-    path("update/<str:username>/", UpdateProfileApiView.as_view(), name="update_profile"),
     path("plan/basic/", BasicPlanListApiView.as_view(), name="basic_plan"),
     path("plan/premium/", PremiumPlanListApiView.as_view(), name="premium_plan"),
     path("plan/enterprise/", EnterprisePlanListApiView.as_view(), name="enterprise_plan"),
     ]
 
 
-"""also should be other plans creating by admin"""
 
--- Upload_Images/apps/profiles/views.py
+++ Upload_Images/apps/profiles/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .exceptions import NotYourProfile, ProfileNotFound
 from .models import Profile
 from .renderers import ProfileJSONRenderer
 from .serializers import ProfileSerializer, UpdatedProfileSerializer
@@ -40,40 +39,6 @@ class GetProfileApiView(APIView):
         serializer = ProfileSerializer(user_profile, context={"request": request})
 
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class UpdateProfileApiView(APIView):
-    """Generic update profile View with PATCH method"""
-    permission_classes = [permissions.IsAuthenticated]
-    renderer_classes = [ProfileJSONRenderer]
-
-    serializer_class = UpdatedProfileSerializer
-
-    def patch(self, request, username):
-
-        """PATCH: The PATCH method is used to apply partial modifications to a resource.
-
-        Generally speaking, partial is used to check whether the fields in the model
-        is needed to do field validation when client submitting data to the view."""
-
-        try:
-            Profile.objects.get(user__username=username)
-        except Profile.DoesNotExist:
-            raise ProfileNotFound
-
-        user_name = request.user.username
-        if user_name != username:
-            raise NotYourProfile
-
-        data = request.data
-        serializer = UpdatedProfileSerializer(instance=request.user.profile, data=data,
-                                              partial=True)
-
-        serializer.is_valid()
-
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
 
 
 
